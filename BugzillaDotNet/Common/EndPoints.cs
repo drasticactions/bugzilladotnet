88f4b4f1d1dc0105bfca0bb8c386a153d23973bb
Tim Miller <innerlogic4321@gmail.com>
2015-12-02 11:07:46 -0500

    Update Endpoints

--- BugzillaDotNet/Common/EndPoints.cs
+++ BugzillaDotNet/Common/EndPoints.cs
@@ -8,26 +8,26 @@ namespace BugzillaDotNet.Common
 {
     public static class EndPoints
     {
-        public static string Bug = "/rest/bug/{0}";
+        public static string Bug = "/bug/{0}";
 
-        public static string BugHistory = "/rest/bug/{0}/history";
+        public static string BugHistory = "/bug/{0}/history";
 
-        public static string BugHistorySince = "/rest/bug/{0}/history?new_since={1}";
+        public static string BugHistorySince = "/bug/{0}/history?new_since={1}";
 
-        public static string BugsIds = "/rest/bug?id={0}";
+        public static string BugsIds = "/bug?id={0}";
 
-        public static string BugsAlias = "/rest/bug?alias={0}";
+        public static string BugsAlias = "/bug?alias={0}";
 
-        public static string BugSearch = "/rest/bug?{0}";
+        public static string BugSearch = "/bug?{0}";
 
-        public static string CreateBug = "/rest/bug";
+        public static string CreateBug = "/bug";
 
-        public static string Login = "/rest/login?login={0}&password={1}";
+        public static string Login = "/login?login={0}&password={1}";
 
-        public static string Logout = "/rest/logout?token={0}";
+        public static string Logout = "/logout?token={0}";
 
-        public static string ValidLogin = "/rest/valid_login?login={0}&token={1}";
+        public static string ValidLogin = "/valid_login?login={0}&token={1}";
 
-        public static string User = "/rest/user";
+        public static string User = "/user";
     }
 }
